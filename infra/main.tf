1aa343c9b7145e4e636db1598ef8b39bfeeea909
Shad Reza <shadreza100@gmail.com>
2025-06-28 15:14:57 +0600

    chore: Renaming the lock table in dynamo db

--- infra/main.tf
+++ infra/main.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_locks" {
-  name         = "tf-state-locking"
+  name         = "${var.project_name}-tf-state-locking"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
